cae85310c467ee11a82fa70f501519ee106ce0fa
satx22 <165065822+satx22@users.noreply.github.com>
2024-07-27 18:57:30 -0500

    Delete jpcoolingheating directory

--- jpcoolingheating/motorofmyapplication/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class MotorOfMyApplicationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jpcoolingheating.motorofmyapplication'
