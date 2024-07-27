b13ae6cac3277c0fae558f18e6c63d45f362c937
satx22 <165065822+satx22@users.noreply.github.com>
2024-07-27 18:21:42 -0500

    Update wsgi.py

--- jpcoolingheating/wsgi.py
+++ jpcoolingheating/wsgi.py
@@ -1,3 +1,4 @@
+  GNU nano 7.2                                                                                          wsgi.py                                                                                                    
 """
 WSGI config for jpcoolingheating project.
 
@@ -14,3 +15,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jpcoolingheating.settings')
 
 application = get_wsgi_application()
+
+
+
+
+
+
+
+
+
