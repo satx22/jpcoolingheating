ba1239951cfc485ee104f767d378727e27bcfbbe
satx22 <jpcoolingandheat@gmail.com>
2024-07-27 21:05:56 -0500

    Initial commit with new code

---
+++ motorofmyapplication/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class MotorOfMyApplicationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jpcoolingheating.motorofmyapplication'
